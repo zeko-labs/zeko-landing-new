312a797d1d22d72030e0289698e0c40db598c998
Hebilicious <xsh4k3@gmail.com>
2025-02-13 21:46:54 +0700

    fix: no-unescaped-entities

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen">
         <h2 className="text-2xl font-bold">404 - Page Not Found</h2>
-        <p>The page you're looking for doesn't exist.</p>
+        <p>{`The page you're looking for doesn't exist.`}</p>
       </div>
     );
   }
